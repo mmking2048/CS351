9303a10d622f6ff3372d609f0ecd2bd4708ce5e3
Michelle Yang <myang22@hawk.iit.edu>
2018-01-28 20:33:11 -0600

    Finished rehash implementation

--- mps/02/hashtable.c
+++ mps/02/hashtable.c
@@ -22,30 +22,15 @@ hashtable_t *make_hashtable(unsigned long size) {
 }
 
 void ht_put(hashtable_t *ht, char *key, void *val) {
+  // remove value if exists
+  ht_del(ht, key);
+
   unsigned int idx = hash(key) % ht->size;
   bucket_t *b = malloc(sizeof(bucket_t));
   b->key = key;
   b->val = val;
   b->next = ht->buckets[idx];
   ht->buckets[idx] = b;
-
-  // Look for existing item and remove it
-  bucket_t *bprev = b;
-  b = b->next;
-
-  while (b) {
-    if (strcmp(b->key, key) == 0) {
-      // remove item
-      bprev->next = b->next;
-      free(b->key);
-      free(b->val);
-      free(b);
-      return;
-    }
-
-    bprev = b;
-    b = b->next;
-  }
 }
 
 void *ht_get(hashtable_t *ht, char *key) {
@@ -95,36 +80,27 @@ void free_hashtable(hashtable_t *ht) {
 }
 
 void  ht_del(hashtable_t *ht, char *key) {
-  unsigned int idx = hash(key) % ht->size;
-  bucket_t *b = ht->buckets[idx];
-
-  // check if first item matches
-  if (strcmp(b->key, key) == 0) {
-    free(b->key);
-    free(b->val);
-    free(b);
-    ht->buckets[idx] = b->next;
-    return;
-  }
-
-  // check through linked list for correct item
-  bucket_t *bprev = b;
-  b = b->next;
-
-  while (b) {
-    if (strcmp(b->key, key) == 0) {
-      // remove item
-      bprev->next = b->next;
-      free(b->key);
-      free(b->val);
-      free(b);
-      return;
-    }
-
-    bprev = b;
-    b = b->next;
-  }
+  
 }
 
 void  ht_rehash(hashtable_t *ht, unsigned long newsize) {
+  // create new hashtable
+  hashtable_t *new_ht = make_hashtable(newsize);
+
+  // iterate through old hashtable and add values to new one
+  bucket_t *b;
+  unsigned long i;
+  for (i=0; i<ht->size; i++) {
+    b = ht->buckets[i];
+    while (b) {
+      ht_put(new_ht, b->key, b->val);
+      b = b->next;
+    }
+  }
+
+  // delete old hashtable
+  free_hashtable(ht);
+
+  // pointer magic
+  *ht = *new_ht;
 }
